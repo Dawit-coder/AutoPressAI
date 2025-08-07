d06950a87efd47b164d955937622e2c7019a8f2a
Dawit-coder <dawit3945@gmail.com>
2025-08-08 00:57:09 +0300

    Set up Inngest client and API route for event-driven functions

---
+++ app/api/inngest/route.ts
@@ -0,0 +1,6 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
+import { functions } from "@/lib/inngest/functions/functions"; 
+
+
+export const{ GET, POST, PUT } = serve({client: inngest, funtions})
