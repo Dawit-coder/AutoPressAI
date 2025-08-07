f696eb42c4f07d2dda80e9d60021192458c8efbe
Dawit-coder <dawit3945@gmail.com>
2025-08-08 02:32:26 +0300

    Implement scheduled newsletter Inngest function and integrate NewsAPI

---
+++ lib/inngest/functions/scheduled-newsletter.ts
@@ -0,0 +1,13 @@
+import { inngest } from "../client";
+import { fetchArticles } from "@/lib/news";
+
+  export default inngest.createFunction({id: "newsletter/scheduled"}, {event: "newsletter.schedule"},
+    async ({event, step, runId}) => {
+
+        const allArticles = await step.run("fetch-news", async () => {
+            const categories = ["technology", "business", "politics"];
+
+            return fetchArticles(categories);
+        })
+    }
+  );
